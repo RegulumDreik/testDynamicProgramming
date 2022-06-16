fcb45bf44a89192f0bc32ceda80fda63ec391a0a
Takada <takada@nous.dev>
2022-06-17 01:51:34 +0600

    Optimized

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
+import sys
 
 if __name__ == '__main__':
-    input()
-    max_weight = int(input())
+    sys.stdin.readline()
+    max_weight = int(sys.stdin.readline())
     if max_weight != 0:
-        weight_list = input().strip().split(' ')
-        weight_list = [int(weight) for weight in weight_list]
-        cost_list = input().strip().split(' ')
-        cost_list = [int(cost) for cost in cost_list]
+        weight_list = sys.stdin.readline().strip().split(' ')
+        weight_list = map(int, weight_list)
+        cost_list = sys.stdin.readline().strip().split(' ')
+        cost_list = map(int, cost_list)
         combined_list = list(zip(weight_list, cost_list))
-        combined_list = sorted(combined_list, key=lambda x: x[1]/x[0], reverse=True)
         mem_table = [[(0, []) for x in range(max_weight+2)]]
-        for item_index in range(1, len(combined_list)+1):
+        previous_line = mem_table[0]
+        for item_index, item in enumerate(combined_list):
             line = [(0, [])]
-            item = combined_list[item_index-1]
-            for weight in range(1, max_weight+2):
-                new_possible_price = item[1] + mem_table[item_index-1][weight - item[0]][0] if weight - item[0] > 0 else 0
-                new_possible_item_list = mem_table[item_index-1][weight - item[0]][1].copy()
-                new_possible_item_list.append(item)
-                if new_possible_price > mem_table[item_index-1][weight][0]:
+            line.extend(previous_line[1:item[0]+1])
+            for weight in range(item[0]+1, max_weight+2):
+                empty_weight_cell = previous_line[weight - item[0]]
+                new_possible_price = item[1] + empty_weight_cell[0]
+                if new_possible_price > previous_line[weight][0]:
+                    new_possible_item_list = empty_weight_cell[1][:]
+                    new_possible_item_list.append(item)
                     line.append((new_possible_price, new_possible_item_list))
                 else:
-                    line.append(mem_table[item_index-1][weight])
+                    line.append(previous_line[weight])
             mem_table.append(line)
+            previous_line = line
         result = mem_table[len(combined_list)][max_weight+1]
-        print(result[0])
-        print(sum(item[0] for item in result[1]))
-        print(len(result[1]))
-        print(' '.join(str(item[0]) for item in result[1]))
-        print(' '.join(str(item[1]) for item in result[1]))
+        weights, costs = zip(*result[1])
+        sys.stdout.write(str(result[0])+'\n')
+        sys.stdout.write(str(sum(weights))+'\n')
+        sys.stdout.write(str(len(result[1]))+'\n')
+        sys.stdout.write(' '.join(map(str, weights))+'\n')
+        sys.stdout.write(' '.join(map(str, costs))+'\n')
     else:
-        print(0)
-        print(0)
-        print(0)
-        print('')
-        print('')
+        sys.stdout.write('0\n')
+        sys.stdout.write('0\n')
+        sys.stdout.write('0\n')
+        sys.stdout.write('\n')
+        sys.stdout.write('\n')
